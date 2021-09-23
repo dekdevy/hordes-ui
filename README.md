3e0705bd9282fde04669838c945acaf7ea280b6e
dek <polydek@googlemail.com>
2021-09-23 15:07:15 +0200

    add rollup

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Hordes.io User Interface
 WIP: The goal of this repo is to create an open source public replacement for the official Hordes.io UI on top of the game client. 
 This will likely take a while to get established and used as a learning project for some folks from the community.
+For now, you can join the small [discord server](http://https://discord.gg/jpYjGv7CTb) for this project which includes a guide on how to get started.
 
-## How to help out
-For now, please join the small [discord server](http://https://discord.gg/jpYjGv7CTb) for this project.
+Running for development, you'll need to run in simultaneously:
+```
+npm run dev-tsc
+npm run dev-rollup
+```
---
+++ rollupconfig.js
@@ -0,0 +1,30 @@
+import alias from '@rollup/plugin-alias'
+
+// in order for rollup to understand aliases, we use plugin-alias, and feed it with the alias info from our tsconfig.js
+import fs from 'fs'
+const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json'))
+const aliasPaths = {}
+for(const a in tsconfig.compilerOptions.paths) {
+  aliasPaths[a.replace('/*', '')] = './dist/ts/'+tsconfig.compilerOptions.paths[a][0].replace('/*', '')
+}
+
+// create plugin chain
+const plugins = [alias({entries: aliasPaths})]
+
+// collect all input and output files
+const files = [
+  {in: 'dist/ts/ui/ui.js', out: 'dist/js/ui/ui.js'},
+  {in: 'dist/ts/server/server.js', out: 'dist/js/server/server.js'}
+]
+
+// unroll files for export
+export default files.map( f => {
+  return {
+    input : f.in,
+    plugins,
+    output: {
+      file  : f.out,
+      format: 'cjs'
+    }
+  }
+})
