fc417639e1d166da3060a3177aaef0cf28e0083d
Tatsuya <antonimontt@gmail.com>
2021-11-16 20:03:10 +0100

    Added stress test icons

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -3,8 +3,8 @@ import * as icon from 'ui/components/basic/icon/icon.js'
 import * as panel from 'ui/components/basic/panel/panel.js'
 
 // just a little more testing
-const maxHp = 300
-const maxMp = 100
+const maxHp = 1900
+const maxMp = 600
 
 //[Tatsuya|15/11/2021] Vars to debug icons P{1.2}
 let stacks = 0
@@ -12,9 +12,36 @@ let cd = 60
 
 const skill = icon.create(document.body, 42, 42, 42, 42)
 icon.set(skill, "Invigorate", cd,stacks)
+//STRESS TEST ICONS
+const skill1 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill1, "Invigorate", cd,stacks)
+const skill2 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill2, "Invigorate", cd,stacks)
+const skill3 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill3, "Invigorate", cd,stacks)
+const skill4 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill4, "Invigorate", cd,stacks)
+const skill5 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill5, "Invigorate", cd,stacks)
+const skill6 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill6, "Invigorate", cd,stacks)
+const skill7 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill7, "Invigorate", cd,stacks)
+const skill8 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill8, "Invigorate", cd,stacks)
+const skill9 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill9, "Invigorate", cd,stacks)
+const skill10 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill10, "Invigorate", cd,stacks)
+const skill11 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill11, "Invigorate", cd,stacks)
+const skill12 = icon.create(document.body, 42, 42, 42, 42)
+icon.set(skill12, "Invigorate", cd,stacks)
 
-const health = bar.create(document.body, 50, screen.height - 300, 300, 30)
-const mana = bar.create(document.body, screen.width - 350, screen.height - 300, 300, 30)
+
+
+const health = bar.create(document.body, 50, 300, 300, 30)
+const mana = bar.create(document.body, 50, 300, 300, 30)
 
 // setup some header and content elements for the panel
 const panelHeader = document.createElement('div')
@@ -25,22 +52,17 @@ const panelContent = document.createElement('div')
 panelContent.innerHTML = `<table><tr>HP</tr><tr>MP</tr><td>${maxHp}</td><td>${maxMp}</td>`
 
 // Draggable, resizable, closable panel
-panel.create(document.body, 100, 100, 300, 300, panelHeader, panelContent, true, true, true)
+panel.create(document.body, 250, 50, 300, 300, panelHeader, panelContent, true, true, true)
 
 // Draggable panel
 const dragHeader = document.createElement('div')
 dragHeader.innerText = 'DRAG ME, carefully!'
-panel.create(document.body, 500, 100, 300, 300, dragHeader, document.createElement('div'), true, false, false)
+panel.create(document.body, 600, 50, 300, 300, dragHeader, document.createElement('div'), true, false, false)
 
 // Resizable panel
 const resizeHeader = document.createElement('div')
 resizeHeader.innerText = 'RESIZE ME, but very very carefully!'
-panel.create(document.body, 800, 400, 300, 200, resizeHeader, document.createElement('div'), false, true, false)
-
-// Closable panel
-const closeHeader = document.createElement('div')
-closeHeader.innerText = 'CLOSE ME, as hard as you want!'
-panel.create(document.body, 300, 700, 250, 200, closeHeader, document.createElement('div'), false, false, true)
+panel.create(document.body, 950, 50, 300, 200, resizeHeader, document.createElement('div'), false, true, false)
 
 let count = 0;
 // tick every frame
@@ -54,6 +76,18 @@ const test = (time: number): void => {
   	if(stacks == 3){stacks = 0}else{stacks++}
   	cd--
   	icon.set(skill, "Invigorate", cd, stacks)
+    icon.set(skill1, "Invigorate", cd, stacks)
+    icon.set(skill2, "Invigorate", cd, stacks)
+    icon.set(skill3, "Invigorate", cd, stacks)
+    icon.set(skill4, "Invigorate", cd, stacks)
+    icon.set(skill5, "Invigorate", cd, stacks)
+    icon.set(skill6, "Invigorate", cd, stacks)
+    icon.set(skill7, "Invigorate", cd, stacks)
+    icon.set(skill8, "Invigorate", cd, stacks)
+    icon.set(skill9, "Invigorate", cd, stacks)
+    icon.set(skill10, "Invigorate", cd, stacks)
+    icon.set(skill11, "Invigorate", cd, stacks)
+    icon.set(skill12, "Invigorate", cd, stacks)
   }
   
   // update bar's
