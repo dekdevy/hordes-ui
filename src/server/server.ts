2b0893795b963f9f6d31d13a6541de61cd250daa
dek <polydek@googlemail.com>
2022-01-02 00:31:02 +0100

    Create server endpoint for test list

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,10 +14,13 @@ interface fReply {
 
 // register static server (allowing to serve files). dist/js/ui folder is served at root
 fastify.register(fastifyStatic, {
-  root: require('path').join(__dirname, '../ui')
+  root: require('path').join(__dirname, '../tests')
 })
 
-// reply with a generic html file which loads ui.js statically
+// the test pages
+const tests  = ['mock', 'list']
+
+// generate root page with list of tests
 fastify.get('/', (req:fRequest, reply:fReply) => {
   reply.type('text/html')
   return reply.send(`
@@ -26,10 +29,30 @@ fastify.get('/', (req:fRequest, reply:fReply) => {
       <title>Hordes.io UI Testbed</title>
       </head>
       <body>
-        <script src='./ui.js'></script>
+        <h3>Test pages:</h3>
+        <ul>
+        ${tests.map(t=>`<li><a href='/${t}'>${t}</a></li>`).join('')}
+        <ul>
       </body>
     </html> 
   `)
+})
+
+// generate end points for test targets
+tests.forEach( test => {
+  fastify.get('/'+test, (req:fRequest, reply:fReply) => {
+    reply.type('text/html')
+    return reply.send(`
+    <html>
+      <head>
+      <title>Hordes.io UI Testbed - ${test}</title>
+      </head>
+      <body>
+        <script src='./${test}.js'></script>
+      </body>
+    </html> 
+  `)
+  })
 })
 
 // run the server
